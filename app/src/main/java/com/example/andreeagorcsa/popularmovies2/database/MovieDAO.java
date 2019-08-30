ee47758930a1115f9cc2076ac2fcb9fc7be77341
andreeagorcsa <andreea.gorcsa@t-systems.com>
2019-08-30 16:38:29 +0200

    30.08.2019 state
    1. Before adding Bind Data library and Support Repository
    
    2. Room + LiveData
    MovieEntity
    MovieDAO
    MovieDatabase
    MovieRepository

---
+++ app/src/main/java/com/example/andreeagorcsa/popularmovies2/database/MovieDAO.java
@@ -0,0 +1,31 @@
+package com.example.andreeagorcsa.popularmovies2.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.andreeagorcsa.popularmovies2.models.Movie;
+
+import java.util.ArrayList;
+
+@Dao
+// DAO =  Data Access Object
+public interface MovieDAO {
+
+    @Insert
+    void insert(Movie movie);
+
+    @Update
+    void update(Movie movie);
+
+    @Delete
+    void delete(Movie movie);
+
+    @Query("SELECT * FROM MOVIE_TABLE WHERE IS_FAVORITE = 1")
+    // the object Movie ArrayList is observed by LiveData
+    // all the changes of the DB will be notified by LiveData which sends the update to the interface
+    LiveData<ArrayList<Movie>> showFavoriteMovies();
+}
